301ba7054af22648c5638e4b3eed3d598b9d270e
Noah <nn8025@onelearn.nz>
2021-05-18 14:56:45 +1200

    code

---
+++ Game.py
@@ -0,0 +1,18 @@
+#imports the pygame module with all functions
+import pygame
+pygame.init()
+#code for game
+window = pygame.display.set_mode((1280,720))
+running = True #game state
+while running:
+    #events since last loop
+    list_of_events = pygame.event.get()
+    #loop through each event
+    for event in list_of_events:
+        print(event)
+        if event.type == pygame.QUIT:
+            running = False
+window.fill((100,100,0))
+pygame.display.update()
+pygame.quit()
+

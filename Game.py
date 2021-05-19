8c97eab451b82977a8c2ea93cbbcde5b66ac6c8a
Noah <nn8025@onelearn.nz>
2021-05-20 10:37:17 +1200

    Box

--- Game.py
+++ Game.py
@@ -1,20 +1,48 @@
-#imports the pygame module with all functions
+#imports the pygame module with all functions and classes
 import pygame
+
 pygame.init()
-#code for game
-window = pygame.display.set_mode((1280,720))
+#the code for your game has to go between these two lines
+
+#create a window object
+window = pygame.display.set_mode((640,480))
+MyRect = pygame.Rect(30,30,100,50)
+#create a game loop
 running = True #game state
+  #color the window background
+window.fill((0,0,0))
+rect = False
 while running:
-    #events since last loop
+    #get all events that have happened since last loop
     list_of_events = pygame.event.get()
-    #loop through each event
+    #loop through each event in the list
     for event in list_of_events:
+        #print each event to console for debuging
         print(event)
         if event.type == pygame.QUIT:
+            #exit the game if x in the corner clicked
             running = False
-window.fill((100,100,0))
-print('hello')
+        elif event.type == pygame.KEYDOWN:
+            #if a key gets pressed
+            if event.key == pygame.K_ESCAPE:
+                #if the escape key is pressed quit the game.
+                running = False
+            if event.key == pygame.K_g and not rect:
+              pygame.draw.rect(window,(130, 252, 100), MyRect)
+              rect = True
+            elif rect and event.key == pygame.K_g:
+               window.fill((0,0,0))  
+               rect = False                       
+            if event.key == pygame.K_w:
+                pass     
+            elif event.key == pygame.K_s:  
+                pass 
+            elif event.key == pygame.K_a:  
+                pass 
+            elif event.key == pygame.K_d:  
+                pass      
+    #update the display
+    pygame.display.update()
 
-pygame.display.update()
+#this code will only run when the loop is stopped
 pygame.quit()
-
